7601e4acaee9adddd37869e86b6805609fb21f83
Thorsten Eurich <thorsten@eurich.de>
2020-11-08 08:51:48 +0100

    no need for payload as parameter

--- check_coiot_validator.go
+++ check_coiot_validator.go
@@ -13,14 +13,13 @@ func main() {
 
 	ip := flag.String("ip", "192.168.178.240", "the Shellys ip address")
 	path := flag.String("path", "/cit/s", "the CoIoT path - /cit/d or /cit/s")
-	payload := flag.String("payload", "", "Payload to send")
 
 	flag.Parse()
 	req := coiot.Message{
 		Type:      coiot.Confirmable,
 		Code:      coiot.GET,
 		MessageID: 12345,
-		Payload:   []byte(*payload),
+		Payload:   []byte(""),
 	}
 	req.SetPathString(*path)
 
